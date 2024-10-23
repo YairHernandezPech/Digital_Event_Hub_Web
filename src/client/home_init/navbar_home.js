d4c9ee292991d69b54fb793541ec7788e37a3f39
unknown <antoniochi90>
2024-10-23 12:31:08 -0600

    UXnavbar

--- src/client/home_init/navbar_home.js
+++ src/client/home_init/navbar_home.js
@@ -1,18 +1,34 @@
 // ClientNavbarHome.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../img/LOGO HUB BLANCO 3.png';
 import '../../styles/navbars.css';
+
 const ClientNavbarHome = ({ user, onLogout }) => {
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para manejar la apertura del menú
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Alternar el estado del menú
+  };
+
   return (
-    <header id="header">
+    <header id="header" className={menuOpen ? 'open' : ''}>
       <div className='container4'>
-        <div id="logo4" class="pull-left">
-
-          <a href="#" class="scrollto4"><img src={logo} alt="" title="" /></a>
+        <div id="logo4" className="pull-left">
+          <a href="#" className="scrollto4"><img src={logo} alt="" title="" /></a>
         </div>
-        <nav id="nav-menu-container">
-          <ul className="nav-menu">
+        <button className="navbar-toggle" onClick={toggleMenu}>
+          <i className="fa fa-bars"></i> {/* Icono de hamburguesa */}
+        </button>
 
+        {/* Botón de cierre "X" */}
+        {menuOpen && (
+          <button className="close-button" onClick={toggleMenu}>
+            <i className="fa fa-times"></i> {/* Icono de cierre */}
+          </button>
+        )}
+
+        <nav id="nav-menu-container">
+          <ul className={`nav-menu ${menuOpen ? 'open' : ''}`}>
             {/* Mostrar los enlaces según si el usuario está autenticado o no */}
             {user ? (
               <>
@@ -27,7 +43,6 @@ const ClientNavbarHome = ({ user, onLogout }) => {
                 <li className="buy-tickets">
                   <a href="#" onClick={onLogout}>Cerrar Sesión</a>
                 </li>
-
               </>
             ) : (
               <>
@@ -35,7 +50,6 @@ const ClientNavbarHome = ({ user, onLogout }) => {
                 <li className="buy-tickets"><a href="/login">Iniciar Sesión <i className="fa-solid fa-right-to-bracket fa-bounce"></i></a></li>
               </>
             )}
-
           </ul>
         </nav>
       </div>
